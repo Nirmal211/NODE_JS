740fd1327eebde14bd0929910d7a56344d95527e
Jasraj Chouhan <106804923+JasrajChouhan@users.noreply.github.com>
2024-11-18 10:44:02 +0000

    (fix): user auth login cookie

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,14 +9,25 @@ const adminAuth = (req, res, next) => {
   }
 };
 
-const userAuth = (req, res, next) => {
-  // Logic of checking if the request is authorized
-  const token = "xyzs";
-  const isUserAuthorized = token === "xyz";
-  if (!isUserAuthorized) {
-    res.status(401).send("User is not authorized");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    const { JWTToken } = req.cookies; // Extract token from cookies
+    if (!JWTToken) {
+      return res.status(401).json({ error: "User authentication token missing" });
+    }
+
+    // const secretKey = process.env.JWT_SECRET; // Use environment variable
+    const secretKey = "GOOGLE$7000&0216" // Please un comment your above line and put secret in .env
+    const decoded = jwt.verify(JWTToken, secretKey); // Verify the JWT token
+    req.user = decoded; // Attach decoded user 
+
     next();
+  } catch (error) {
+    console.error("Error in userAuth:", error);
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
